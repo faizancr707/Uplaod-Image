af232c4be9484ee0871294aa371ac5d3c3a0b9dc
Faizan Alam <fa4393837@gmail.com>
2025-12-22 12:24:23 +0530

    Uplaod Image

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import './App.css'
-import Home from './routes/Home'
-import Call from './routes/Call'
-import About from './routes/About'
+import Image from './image.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/call">Call</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/call" element={<Call />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <div>
+      <Image />
+    </div>
+  );
 }
 
-export default App
+export default App;
